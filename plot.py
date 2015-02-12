fc866e07e4e73d33faa1002f199ea32fabdb7c71
Peter H. Froehlich <peter.hans.froehlich@gmail.com>
2015-02-12 17:15:01 -0500

    Simple plot example.

---
+++ plot.py
@@ -0,0 +1,13 @@
+# from the pylab documentation
+
+from pylab import *
+
+t = arange(0.0, 2.0, 0.01)
+s = sin(2*pi*t)
+plot(t, s)
+
+xlabel('time (s)')
+ylabel('voltage (mV)')
+title('About as simple as it gets, folks')
+grid(True)
+savefig("plot.pdf")
